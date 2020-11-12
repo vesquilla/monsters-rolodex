6e2f39cb83878744243b5fedf2ede073c1c0ff59
Virg Esquilla <virgesquillajr@gmail.com>
2020-11-12 23:15:21 +0800

    Updates

---
+++ precache-manifest.a12fde48d5c11ad0ddb56cb45353453b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de841a5f2cf0d9ea9a13992ebee85fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "20ec949a95c920df6449",
+    "url": "/monsters-rolodex/static/css/main.59c05ede.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ec949a95c920df6449",
+    "url": "/monsters-rolodex/static/js/main.d098e292.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
